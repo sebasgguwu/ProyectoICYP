702e8dc137307dee227d10795692a083233172d0
sebasgguwu <122837837+sebasgguwu@users.noreply.github.com>
2023-12-08 01:26:29 -0600

    Commit 3.0 / Lectura de datos
    Lectura de información a través de scanf

--- CYP05.cpp
+++ CYP05.cpp
@@ -31,3 +31,19 @@ void burbujaInt(int arr[], int numDatos)
 //				arr[comp + 1] = aux;
 //			}
 //}
+int main()
+{
+	int valor[10];
+	float valor2[10][10];
+	float aux[10];
+	int numElem;
+	int numAlum=0;
+	scanf_s("%i", &numElem); //Numero de alumnos
+	char nombre[5][longi];
+	int i;
+	for (int i = 0; i < numElem; i++)
+	{
+		printf("No. de cuenta: ");
+		scanf_s("%i", &valor[i]);
+		scanf_s("%f", &valor2[i]);
+	}
